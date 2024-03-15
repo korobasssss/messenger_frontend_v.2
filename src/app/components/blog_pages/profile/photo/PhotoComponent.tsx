1466ba4d31b04834bfe09676cebe67cf8ac8c7d6
Anastasia <an.korobeynikova@gmail.com>
2024-03-16 02:10:38 +0300

    photo page and design

--- src/app/components/blog_pages/profile/photo/PhotoComponent.tsx
+++ src/app/components/blog_pages/profile/photo/PhotoComponent.tsx
@@ -1,7 +1,31 @@
+import photo_scss from '@/app/scss/for_components/blog_pages/photo/photo.module.scss'
+import '@/app/scss/global/globals.scss'
+
+import back_icon from '@/assets/icons/photo/back_icon.svg'
+import default_photo from '@/assets/icons/post/default_post_icon.svg'
+import Image from "next/image";
+
 export const PhotoComponent = () => {
     return (
-        <main>
-
+        <main className={photo_scss.page}>
+            <header className={photo_scss.header}>
+                <Image src={back_icon} alt={'back icon'}/>
+                <section className={photo_scss.userData}>
+                    <div className={'name_small'}>Имя</div>
+                    <div className={'nickname'}>6 фото</div>
+                </section>
+            </header>
+            <ul className={photo_scss.photo}>
+                <li>
+                    <Image src={default_photo} alt={'user photo'}/>
+                </li>
+                <li>
+                    <Image src={default_photo} alt={'user photo'}/>
+                </li>
+                <li>
+                    <Image src={default_photo} alt={'user photo'}/>
+                </li>
+            </ul>
         </main>
     )
 }
